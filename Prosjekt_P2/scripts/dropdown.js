21581f6d71273e0e5fd2f32175eab2a34b8912f7
Christoffer Lofsberg <Christoffer@dhcp-015089.wlan.ntnu.no>
2015-11-16 11:35:44 +0100

    Fixed dropdown

--- Prosjekt_P2/scripts/dropdown.js
+++ Prosjekt_P2/scripts/dropdown.js
@@ -12,30 +12,32 @@ var date = new Date()
 //Iterate over all dropdown elements
 for(var i = 0; i < dropdowns.length; i++) {
 	//Add eventlistener to each dropdown both mouseover and mouseout
+	var ul = dropdowns[i].getElementsByTagName("ul")[0]
+	var a = dropdowns[i].getElementsByTagName("a")[0]
 
 	dropdowns[i].addEventListener("mouseover", function(){
 
 		//Display the submenu at DOM, also change backgroundcolor at link object
-		this.childNodes[3].style.display = "block"
-		this.childNodes[1].style.backgroundColor = "#CCE1F2"
+		ul.style.display = "block"
+		a.style.backgroundColor = "#CCE1F2"
 		
 		//Add a mouseover event to the submenu ul as well so it stays
-		this.childNodes[3].addEventListener("mouseover", function(){
+		ul.addEventListener("mouseover", function(){
 			this.style.display = "block"
 		})
 	})
 
 	dropdowns[i].addEventListener("mouseout", function(){
 		//removing the submenu ul
-		this.childNodes[3].style.display = "none"
+		ul.style.display = "none"
 		//Since we have daynight.js and therefore different colors as background on menu
 		//Set the background color at the link object back to its original state
 		if(date.getHours() >= 20 || date.getHours() < 8) {
 			//Night color
-			this.childNodes[1].style.backgroundColor = "#7694A8"
+			a.style.backgroundColor = "#7694A8"
 		} else {
 			//Day color
-			this.childNodes[1].style.backgroundColor = "#B1CEE6"
+			a.style.backgroundColor = "#B1CEE6"
 		}
 	})
 }
